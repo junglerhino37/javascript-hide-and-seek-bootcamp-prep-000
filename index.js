e06453d661e0b6da870e05b9382e70f28ffeb57e
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-27 20:16:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector){
+ var ans =document.querySelector(selector)
+ return ans
+}
+
+function nestedTarget(){
+  var ans;
+  ans=document.querySelector('#nested .target')
+  return ans
+}
+
+function increaseRankBy(n){
+const array=document.querySelectorAll('.ranked-list li')
+ for (i=0;i<array.length;i++){
+   
+   array[i].innerHTML= (parseInt(array[i].innerHTML)+n).toString()
+ }
+}
+
+deepestChild()
+
+function deepestChild(){
+  var lis=document.getElementById('grand-node').querySelectorAll('div')
+return lis[lis.length-1]
+}
